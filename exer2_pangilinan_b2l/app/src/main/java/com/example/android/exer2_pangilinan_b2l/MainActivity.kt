1b4f6f6ccabc1e0ca4215a65630b04634a6b07d9
Erl Jhon <efpangilinan1@up.edu.ph>
2020-02-04 19:00:56 +0800

    EXER and Day2 CMSC 23

---
+++ exer2_pangilinan_b2l/app/src/main/java/com/example/android/exer2_pangilinan_b2l/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.android.exer2_pangilinan_b2l
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
